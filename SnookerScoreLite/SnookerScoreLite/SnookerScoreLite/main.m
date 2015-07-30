ee25903a68cde643ae4e81fc572bbc4a247cadb6
Andrew Glew <drewglew@gmail.com>
2015-07-30 23:14:25 +0200

    New objects

---
+++ SnookerScoreLite/SnookerScoreLite/SnookerScoreLite/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SnookerScoreLite
+//
+//  Created by andrew glew on 22/07/2015.
+//  Copyright (c) 2015 andrew glew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
